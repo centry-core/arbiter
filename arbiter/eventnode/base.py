64857e92983711bb2a4e0fff4441bb7c5311c2ce
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2025-04-16 12:20:29 +0300

    EventNode: base-experiment: emit via queue

--- arbiter/eventnode/base.py
+++ arbiter/eventnode/base.py
@@ -57,7 +57,9 @@ class EventNodeBase:  # pylint: disable=R0902
         self.stop_event = threading.Event()
         self.event_lock = threading.Lock()
         self.sync_queue = queue.Queue()
+        self.emit_queue = queue.Queue()
         #
+        self.emitting_thread = threading.Thread(target=self.emitting_worker, daemon=True)
         self.listening_thread = threading.Thread(target=self.listening_worker, daemon=True)
         self.callback_threads = []
         for _ in range(callback_workers):
@@ -80,6 +82,8 @@ class EventNodeBase:  # pylint: disable=R0902
         """ Start event node """
         if self.started:
             return
+        #
+        self.emitting_thread.start()
         #
         if emit_only:
             self.ready_event.set()
@@ -133,7 +137,7 @@ class EventNodeBase:  # pylint: disable=R0902
             return
         #
         data = self.make_event_data(event_name, payload)
-        self.emit_data(data)
+        self.emit_queue.put(data)
 
     def make_event_data(self, event_name, payload=None):
         """ Make event data """
@@ -183,6 +187,16 @@ class EventNodeBase:  # pylint: disable=R0902
         with self.event_lock:
             while hook in self.after_callback_hooks:
                 self.after_callback_hooks.remove(hook)
+
+    def emitting_worker(self):  # pylint: disable=R0912
+        """ Emitting thread: emit data """
+        while self.running:
+            try:
+                data = self.emit_queue.get()
+                self.emit_data(data)
+            except:  # pylint: disable=W0702
+                if self.log_errors:
+                    log.exception("Error during event emitting, skipping")
 
     def callback_worker(self):  # pylint: disable=R0912
         """ Callback thread: call subscribers """
